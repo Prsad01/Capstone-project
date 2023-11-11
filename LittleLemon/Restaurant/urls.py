1759c2d44a9e69b218984b884829993fff474bda
unknown <prasadmande01@gmail.com>
2023-11-11 13:21:18 +0530

    3rd push after creating a views and serializer for menu and booking model

--- LittleLemon/Restaurant/urls.py
+++ LittleLemon/Restaurant/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls import path,include
+from .views import index , UserView,MenuView, BookingView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter(trailing_slash=False)
+router.register('users',UserView,basename='user')
+router.register('booking/tables',BookingView, basename='tables')
+router.register('menu-item',MenuView, basename='menu')
 
 urlpatterns = [
-    path('', views.index,name='path')
-]
+#    path('', include('router.urls'))
+]+router.urls
